2b4195f6c4f6fac5130769f56e81ed99b9cea7be
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2024-01-03 12:39:39 +0100

    Add icecream debugging and update requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from icecream import ic
+
+from email_analyse.ConfirmationEmail import ConfirmationEmail
+from email_analyse.PasswordEmail import PasswordEmail
 from email_service.EmailManager import EmailManager
 
 if __name__ == "__main__":
     with EmailManager() as email_importer:
         for email in email_importer.emails():
-            print(email)
+            ic(ConfirmationEmail.check(email))
+            ic(PasswordEmail.check(email))
