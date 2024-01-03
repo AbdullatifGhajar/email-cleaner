b5567c239d222246ce399d6fa869753f3863ac56
Abdullatif Ghajar <abdullatif.ghajar@student.uni-potsdam.hpi.de>
2024-01-03 11:24:20 +0100

    Refactor email manager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from email_service.EmailImporter import EmailImporter
+from email_service.EmailManager import EmailManager
 
 if __name__ == "__main__":
-    with EmailImporter() as email_importer:
+    with EmailManager() as email_importer:
         for email in email_importer.emails():
             print(email)
